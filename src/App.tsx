076c0fe77f5df3a8fcacad2554644a95ed8dafe1
LucasAdamyMartin <lucasadamy123@gmail.com>
2025-01-30 19:00:30 +0100

    avancement du header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from './components/Button';
+import { ChatBubbleOvalLeftEllipsisIcon, HeartIcon, ShoppingBagIcon } from "@heroicons/react/24/outline";
 
 function App() {
   const text = "Sign In";
@@ -7,11 +8,26 @@ function App() {
   <div>
     <div className="h-8.5 w-screen bg-gray-100"></div>
     <div className="flex h-32 w-screen">
-      <div className="flex justify-end w-full h-full">
-        <div className="w-max">
-          <Button text={text} variant='outline'></Button>
-          <Button text={text2} variant='inline'></Button>
-        </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <nav className="flex justify-between items-center h-fit w-full m-15  space-x-8">
+          <h1 className="font-pacifico text-[40px]">vantela</h1>
+          <ul className='flex space-x-12 justify-center flex-grow ml-15'>
+            <li>Home</li>
+            <li>Sneakers</li>
+            <li>Slip On</li>
+            <li>Sandals</li>
+            <li>Others</li>
+          </ul>
+          <div className='flex space-x-2 col-span-1'>
+            <ChatBubbleOvalLeftEllipsisIcon className='size-7 text-primary'></ChatBubbleOvalLeftEllipsisIcon>
+            <HeartIcon className='size-7 text-primary'></HeartIcon>
+            <ShoppingBagIcon className="size-7 text-primary"></ShoppingBagIcon>
+          </div>
+          <div className='flex space-x-4 col-span-2'>
+            <Button text={text} variant='outline'></Button>
+            <Button text={text2} variant='inline'></Button>
+          </div>
+        </nav>
       </div>
     </div>
   </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,5 +3,5 @@ export function Button({text, variant} : Readonly<ButtonProps>) {
     const variantStyle = variant === 'outline' 
     ? { backgroundColor: '#1F3E97', color:'white'} 
     : { color: '#1F3E97', backgroundColor:'white', border: '2px solid #1F3E97'};
-    return <button className="font-bold h-[39px] w-[78px] text-[12px] font-inter rounded-full m-2" style={variantStyle}>{text}</button>
+    return <button className="font-bold h-[39px] w-[78px] text-[12px] font-inter rounded-full" style={variantStyle}>{text}</button>
 }
