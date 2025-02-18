324a66e73de545570443a3b7c78fac6aea2c378d
LucasAdamyMartin <lucasadamy123@gmail.com>
2025-02-18 19:11:12 +0100

    feat(signUp) : ceate forms

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,18 @@
 import { TEXTS } from "../../utils/constants";
 import { ButtonClick } from "../components/Button/ButtonClick";
-import { Form } from "../components/Form";
+import { ButtonNavigation } from "../components/Button/ButtonNavigation";
+import { Field, Form } from "../components/Form";
 import { Text } from "../components/Text";
 
 export function SignUp() {
+    const contactFields: Field[] = [
+        { name: "name", label: "Full name", type: "text", required: true },
+        { name: "email", label: "Email", type: "email", required: true },
+        { name: "message", label: "Message", type: "textarea", required: true },
+      ];
+    const handleRegisterSubmit = (data: Record<string, string>) => {
+        console.log("Données d'inscription :", data);
+      };
     const containerClasses =
     "flex flex-col rounded-2xl lg:bg-[url('/img/img_SignUp.png')] bg-[url('/img/chaussure_hero_mobile.webp')] bg-cover bg-center w-[382px] h-[452px]";
     return (
@@ -14,9 +23,9 @@ export function SignUp() {
         <div className="flex flex-row space-x-10">
             <div className="flex flex-col space-y-6">
                 <div className="flex border-b-2 border-gray-300">
-                    <Form/>
+                    <Form fields={contactFields} onSubmit={handleRegisterSubmit}/>
                 </div>
-                <Form/>
+                <Form fields={contactFields} onSubmit={handleRegisterSubmit}/>
                 <div className="flex flex-row items-center space-x-2">
                     <ButtonClick height="h-[24px]" text="" width="w-[24px]" additionalClassName="rounded-lg bg-gray-300"/>
                     <Text color="text-black" fontSize="text-[14px]" fontStyle="font-Inter" text={TEXTS.ConditionsText}/>
@@ -35,13 +44,14 @@ export function SignUp() {
                <div className="flex pt-4 pl-4">
                     <Text color="text-primary" fontSize="text-[22px]" fontStyle="font-pacifico" text="vantela"/>
                 </div>
-                <div className="flex-grow items-end">
-                    <Text color="text-white" fontSize="text-[30px]" fontStyle="font-Inter" text="Easy shopping" additionalClassName="font-bold -rotate-12"/>
-                    <div className="flex flex-row space-x-2 -rotate-12">
-                        <Text color="text-white" fontSize="text-[30px]" fontStyle="font-Inter" text="just from" additionalClassName="font-bold"/>
-                        <Text color="text-secondary" fontSize="text-[30px]" fontStyle="font-Inter" text="home" additionalClassName="font-bold"/>
+                <div className="flex flex-col justify-end items-center flex-grow pb-10">
+                    <Text color="text-white" fontSize="text-[36px]" fontStyle="font-Inter" text="Easy shopping" additionalClassName="font-bold -rotate-6"/>
+                    <div className="flex flex-row space-x-2 -rotate-6">
+                        <Text color="text-white" fontSize="text-[36px]" fontStyle="font-Inter" text="just from" additionalClassName="font-bold"/>
+                        <Text color="text-secondary" fontSize="text-[36px]" fontStyle="font-Inter" text="home" additionalClassName="font-bold"/>
                     </div>
                </div>
+               <ButtonNavigation colorOnClick="bg-white" containerClasses="flex flex-row justify-end pr-2 pb-2 space-x-2"/>
             </div>
         </div>
     </div>
